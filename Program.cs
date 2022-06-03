1ec4acb03c05ce0b0287acb3af13e6b3ce801afd
faevi <94059832+faevi@users.noreply.github.com>
2022-06-03 17:16:41 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,3 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿    // See https://aka.ms/new-console-template for more information
+    using System;
+    using System.Collections.Concurrent;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    namespace Giraffe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(GetDay(2));
+
+            Console.ReadLine();
+        }
+        static string GetDay(int dayNum)
+        {
+            string dayName;
+            switch(dayNum)
+            {
+                case 0:
+                    dayName = "Sunday";
+                    break;
+                case 1:
+                    dayName = "Monday";
+                        break;
+                default:
+                    dayName = "Invalid Number";
+                    break;
+            }
+            return dayName;
+
+        }
+    }
+}
+
 
