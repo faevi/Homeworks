a9a8d8ddbc2d18d8c1c89a16bb51e839251000e0
faevi <94059832+faevi@users.noreply.github.com>
2022-06-12 17:40:58 +0300

    homeWork2

---
+++ HashTable.cs
@@ -0,0 +1,120 @@
+﻿namespace HomeWork2
+{
+    public class HashTable<T> : newLinkedList<T> // Task 2
+
+    {
+        //Т.к. вместо массива у меня мой слепленный связанный список, то рехеш и ресайз не нужен))0))0)
+        //private newLinkedList<T> list = new newLinkedList<T>();
+        //private double rehash_size = 0.75;
+        //private int defaultSize = 8;
+
+        int HashFunction1(T value, int table_size)
+        {
+            {
+                return HashFunction(value, table_size, table_size - 1);
+            }
+        }
+
+        int HashFunction2(T value, int table_size)
+        {
+            {
+                return HashFunction(value, table_size, table_size + 1);
+            }
+        }
+
+
+        public int HashFunction(T? value, int table_size, int key)
+        {
+            int p = 31; //Простое число
+            int rez = 0; //Результат вычисления
+            string stringValue = Convert.ToString(value);
+            for (int i = 0; i < Convert.ToString(value).Length; i++)
+            {
+                rez += (int)Math.Pow(p, stringValue.Length - 1 - i) * (int)(stringValue[i]);//Подсчет хеш-функции
+            }
+            return rez;
+        }
+
+        public bool Find(T value)
+        {
+            int h1 = HashFunction1(value, nodesCount); // значение, отвечающее за начальную позицию
+            int h2 = HashFunction2(value, nodesCount); // значение, ответственное за "шаг" по таблице
+            int i = 0;
+
+            while (h1 < nodesCount && i < nodesCount)
+            {
+                Node<T> temp = GetNode(h1); // Чтобы два раза по листу не бегать в след if
+
+                //Console.WriteLine("temp.field: {0}, temp position: {1}, value: {2}, temp state: {3}", temp.field, h1, value, temp.state);
+                if (temp.field != null && temp.field.Equals(value) && temp.state)
+                {
+                    return true; // такой элемент есть
+                }
+
+                h1 = (h1 + h2) % nodesCount;
+                i++;
+                //Console.WriteLine("h1: {0}, nodesCount: {1}", h1, list.nodesCount);
+            }
+            return false;
+        }
+
+        public new bool Remove(T value) // потому что в родительском определен метод удаляющий по позиции
+        {
+            int h1 = HashFunction1(value, nodesCount); // значение, отвечающее за начальную позицию
+            int h2 = HashFunction2(value, nodesCount); // значение, ответственное за "шаг" по таблице
+
+            while (h1 < nodesCount)
+            {
+                Node<T> temp = GetNode(h1); // Чтобы два раза по листу не бегать в след if
+
+                if (temp.field!.Equals(value) && temp.state)
+                {
+                    temp.state = false;
+                    emptyCount++;
+                    return true; // такой элемент есть
+                }
+
+                h1 = (h1 + h2) % nodesCount;
+            }
+
+            return false;
+        }
+
+        public bool Add(T value)
+        {
+            int h1 = HashFunction1(value, nodesCount); // значение, отвечающее за начальную позицию
+            int h2 = HashFunction2(value, nodesCount); // значение, ответственное за "шаг" по таблице
+
+            while (h1 < nodesCount)
+            {
+                Node<T> temp = GetNode(h1); // Чтобы два раза по листу не бегать в след if
+
+                if (!temp.state)
+                {
+                    Add(value, h1);
+                    return true; // такой элемент есть
+                }
+
+                h1 = (h1 + h2) % nodesCount;
+            }
+
+            Add(value, h1);
+            return true;
+        }
+
+        public static void ShowInfo(HashTable<T> table)
+        {
+            bool isEmpty = table.nodesCount > 0 ? false : true;
+            if (isEmpty)
+            {
+                Console.WriteLine("The table is empty!");
+            }
+            else
+            {
+                Console.WriteLine("Current table has {0} elements, {1} elements is real, {2} elements is empty",
+                    table.nodesCount, table.nodesCount - table.emptyCount, table.emptyCount);
+            }
+            ShowLinnkedList(table);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,119 @@
+﻿
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            Random random = new Random();
+            int N = 9;
+            //Task 1
+            Console.WriteLine("\n\nTask 1 Factorial \n");
+            for (int i = 0; i < N; i++)
+            {
+                Console.Write(Task1.Factorial(i) + " ");
+            }
+            //Task 2
+            Console.WriteLine("\n \nTask 2 Fibbonaci \n");
+            for (int i = 0; i < N; i++)
+            {
+                Console.Write(Task1.Fibonnaci(i) + " ");
+            }
+            //Task 3
+            Console.WriteLine("\n \n Task 3 Sort\n");
+            double[] a3 = new double[N];
+            for (int i = 0; i < N; i++)
+            {
+                a3[i] = random.Next(0, 100);
+                Console.Write(a3[i] + " ");
+            }
+            Task1.QuickSort(a3, 0, N - 1);
+            Console.WriteLine("\n QuickSort\n");
+            for (int i = 0; i < N; i++)
+            {
+                Console.Write(a3[i] + " ");
+            }
+
+            //Task 4
+            Console.WriteLine("\n \n Task 4 HelixShow\n");
+
+            double[,] a4 = new double[N, N];
+
+
+            for (int i = 0; i < N; i++)
+            {
+                for (int j = 0; j < N; j++)
+                {
+                    a4[i, j] = random.Next(100);
+
+                }
+
+            }
+            Task1.ShowArray(a4);
+            Task1.ShowArrayHelix(a4);
+
+
+
+            //Task 5
+            Console.WriteLine("\n\n\nTask 5 Sort first table\n");
+
+            Task1.MatrixSort(a4);
+            Task1.ShowArray(a4);
+            */
+            //HashTable<string> table = new HashTable<string>();
+            //for (int i = 0; i < 10; i++)
+            //{
+            //    //Console.WriteLine("Add element: {0}", i);
+            //    table.Add(Convert.ToString(i));
+            //}
+            //HashTable<string>.ShowInfo(table);
+            //Console.WriteLine(table.Find("0"));
+            //Console.WriteLine(table.Remove("0"));            
+            //Console.WriteLine(table.Find("0"));
+            //Console.WriteLine(table.Find("3"));
+            //Console.WriteLine(table.Remove("3"));
+            //Console.WriteLine(table.Find("3"));
+            //Console.WriteLine(table.Remove("100"));
+            //Console.WriteLine(table.Find("100"));
+            //HashTable<string>.ShowInfo(table);
+
+
+            //Task 1
+            newLinkedList<int> list = new newLinkedList<int>();
+            
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("Add in list element: {0}", i);
+                list.Add(i,i);
+            }
+            
+            list.Add(50, 15);
+            newLinkedList<int>.ShowInfo(list);
+
+            //Task 2
+
+            HashTable<string> table = new HashTable<string>();
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("Add in table element: {0}", i);
+                table.Add(Convert.ToString(i));
+            }
+            HashTable<string>.ShowInfo(table);
+
+            //Task 3
+            Console.WriteLine(StackCalculator.DoCallculation("1+3-(5-10)*4"));
+            Console.WriteLine(StackCalculator.DoCallculation("(-1157+24)/17*3"));
+            //StackCalculator.ShowOutStack();
+            //StackCalculator.CreateStack("-1.5+(-3-(5-10)*4)");
+            //StackCalculator.ShowOutStack();
+            //StackCalculator.CreateStack("(1+3)-(5-10)*4");
+            //StackCalculator.ShowOutStack();
+
+        }
+
+
+
+    }
+}
