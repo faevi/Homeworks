18b41c73cacb8308cf53c02aea0153f5f3ed629b
faevi <gleb-levin323@mail.ru>
2022-06-30 19:35:25 +0300

    Домашняя работа 4

---
+++ Clock/Clock/MainWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Windows.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Clock
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            _timer = new DispatcherTimer(new TimeSpan(0, 0, 1), DispatcherPriority.Normal, delegate
+            {
+                timeNow.Text = DateTime.Now.ToString("HH:mm:ss");
+            }, Dispatcher);
+        }
+        private DispatcherTimer _timer;
+    }
+}
