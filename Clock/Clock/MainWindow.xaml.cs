49bef6a88b3277af9eccb8046d36ecc97fcfdb1f
faevi <gleb-levin323@mail.ru>
2022-07-12 11:09:08 +0300

    Исправления в часах

--- Clock/Clock/MainWindow.xaml.cs
+++ Clock/Clock/MainWindow.xaml.cs
@@ -1,24 +1,13 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading;
 using System.Windows.Threading;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Clock
 {
     public partial class MainWindow : Window
     {
+        private DispatcherTimer _timer;
+         
         public MainWindow()
         {
             InitializeComponent();
@@ -28,6 +17,6 @@ namespace Clock
                 timeNow.Text = DateTime.Now.ToString("HH:mm:ss");
             }, Dispatcher);
         }
-        private DispatcherTimer _timer;
+        
     }
 }
