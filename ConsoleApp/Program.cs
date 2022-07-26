2f67eed8c008425c9ebcd9498d4e222576001453
faevi <gleb-levin323@mail.ru>
2022-07-26 09:37:02 +0300

    Домашняя работа 5

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -4,10 +4,6 @@
     {
         static void Main(string[] args)
         {
-            BinaryTree<int> tree = new BinaryTree<int>();
-            tree.Insert(0, 0);
-            tree.Insert(5, 5);
-
 
         }
     }
