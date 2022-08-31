07453918f08a174eaf3dfc301ed40d6eb8b8c841
faevi <gleb-levin323@mail.ru>
2022-08-31 18:44:26 +0300

    HomeWork9

---
+++ ConsoleApp/LazyFactory.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    public class LazyFactory<T>
+    {
+        public static ILazy<T> CreateMultithreadingLazy<T>(Func<T> supplier)
+        {
+            return new LazyMultithreading<T>(supplier);
+        }
+    }
+}
---
+++ ConsoleApp/LazyMultithreading.cs
@@ -0,0 +1,35 @@
+﻿namespace ConsoleApp
+{
+    public class LazyMultithreading<T> : ILazy<T>
+    {
+        private T _value;
+        private bool _valueExist = false;
+        private Func<T>? _function;
+
+        public LazyMultithreading(Func<T>? function)
+        {
+            _function = function;
+        }
+
+        public T Get()
+        {
+            AutoResetEvent waitHandler = new AutoResetEvent(true);
+
+            if (!_valueExist)
+            {
+                waitHandler.WaitOne();
+                _valueExist = true;
+
+                if (_function == null)
+                {
+                    return default(T);
+                }
+
+                _value = _function();
+                waitHandler.Set();
+            }
+
+            return _value;
+        }
+    }
+}
