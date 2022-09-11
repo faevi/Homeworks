35780215775eea927715336b05c5840630c85cac
faevi <gleb-levin323@mail.ru>
2022-09-11 15:51:12 +0300

    Домашка 10 без тестов

---
+++ ConsoleApp/MyTask.cs
@@ -0,0 +1,75 @@
+﻿using System.Threading;
+
+namespace ConsoleApp
+{
+    public class MyTask<TResult> : IMyTask<TResult>
+    {
+        private Func<TResult> _function;
+        private TResult _result;
+        private ManualResetEvent _waitHandle;
+        private MyThreadPool _threadPool;
+        private Queue<Action> _taskQueue;
+        private bool _isComplete = false;
+        private object locker;
+        public MyTask(Func<TResult> function, MyThreadPool threadPool)
+        {
+            _threadPool = threadPool;
+            _taskQueue = new Queue<Action>();
+            _function = function;
+        }
+
+        public bool IsComplete 
+        {
+            get => _isComplete;
+            set => _isComplete = value;
+        }
+
+        public TResult Result
+        {
+            get
+            {
+                _waitHandle.WaitOne();
+                return _result;
+            }
+        }
+
+        public void DoTask()
+        {
+            lock (locker)
+            {
+                try
+                {
+                    _result = _function();
+                }
+                catch (Exception ex)
+                {
+                    throw new AggregateException(ex);
+                }
+                finally
+                {
+                    IsComplete = true;
+                    _waitHandle.Set();
+                    while(_taskQueue.Count() != 0)
+                    {
+                        _threadPool.AddAction(_taskQueue.Dequeue());
+                    }
+                }
+            }            
+        }
+
+        public IMyTask<TNewResult> ContinueWith<TNewResult>(Func<TResult, TNewResult> function)
+        {
+            MyTask<TNewResult> newTask = new MyTask<TNewResult>(() => function(_result), _threadPool);
+            
+            if (IsComplete)
+            {
+                _threadPool.AddTask(newTask);
+            } else
+            {
+                _taskQueue.Enqueue(() => newTask.DoTask());
+            }
+
+            return newTask;            
+        }
+    }
+}
---
+++ ConsoleAppTests/ConsoleAppTests/MyThreadPoolTests.cs
@@ -0,0 +1,28 @@
+﻿using ConsoleApp;
+
+namespace ConsoleAppTests
+{
+    [TestClass]
+    public class MyThreadPoolTests
+    {
+        [TestMethod]
+        public void FirstMyThreadPoolTest()
+        {
+            List<MyTask<int>> taskList = new List<MyTask<int>>();
+            MyThreadPool testPool = new MyThreadPool(4);
+            for (int i = 0; i < 100; i++)
+            {
+                MyTask<int> testTask = new MyTask<int>(() => i*i, testPool);
+                taskList.Add(testTask);
+            }
+
+            for (int i = 0; i < 100; i++)
+            {
+                MyTask<int> testTask = new MyTask<int>(() => i*i, testPool);
+                taskList.Add(testTask);
+            }
+
+
+        }
+    }
+}
