f858de47da1176f7dd02a921e62d8d9525899eb6
faevi <gleb-levin323@mail.ru>
2022-09-17 14:28:59 +0300

    HomeWork11

---
+++ FirstWebApplication/FirstWebApplication/AppContext/DataContext.cs
@@ -0,0 +1,12 @@
+﻿using FirstWebApplication.Models;
+using Microsoft.EntityFrameworkCore;
+
+public class DataContext : DbContext
+{
+    public DbSet<DataModel> DataSet { get; set; } = null!;
+    public DataContext(DbContextOptions<DataContext> options)
+        : base(options)
+    {
+        Database.EnsureCreated();   // создаем базу данных при первом обращении
+    }
+}
---
+++ FirstWebApplication/FirstWebApplication/Controllers/HomeController.cs
@@ -0,0 +1,65 @@
+﻿using FirstWebApplication.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace FirstWebApplication.Controllers
+{
+    public class HomeController : Controller
+    {
+        DataContext db;
+        public HomeController(DataContext context)
+        {
+            db = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            return View(await db.DataSet.ToListAsync());
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(DataModel importData)
+        {
+            db.DataSet.Add(importData);
+            await db.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }        
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            return View(await db.DataSet.ToListAsync());
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Get(int? id)
+        {
+            if (id != null)
+            {
+                DataModel? outputData = await db.DataSet.FirstOrDefaultAsync(p => p.Id == id);
+                if (outputData != null)
+                {
+                    ViewBag.Data = outputData;
+                    return View("SingleDataView");
+                }                
+            }
+            return NotFound();
+        }
+
+        public async Task<IActionResult> BackToIndex()
+        {
+            return RedirectToAction("Index");
+        }
+    }
+}
